1f9f2dcc89409d5cef04ab625d8b89b4d287ea93
duruixue <duruixue@zjseek.com.cn>
2014-10-29 12:01:11 +0800

    update Logger

--- lib_anchorer/src/main/java/com/anchorer/lib/utils/L.java
+++ lib_anchorer/src/main/java/com/anchorer/lib/utils/L.java
@@ -9,20 +9,43 @@ import android.util.Log;
  */
 public class L {
 
-    public static volatile boolean DEBUG = true;
+    private static int level;
+
+    public static final int LEVEL_VERBOSE = Log.VERBOSE;
+    public static final int LEVEL_DEBUG = Log.DEBUG;
+    public static final int LEVEL_INFO = Log.INFO;
+    public static final int LEVEL_WARN = Log.WARN;
+    public static final int LEVEL_ERROR = Log.ERROR;
+    public static final int LEVEL_NONE = 100;
 
     /**
      * Enable Logger
+     * @param level Log Level
+     */
+    public static void enableLogging(int level) {
+        L.level = level;
+    }
+
+    /**
+     * Enable Logger: default level verbose
      */
     public static void enableLogging() {
-        DEBUG = true;
+        enableLogging(LEVEL_VERBOSE);
+    }
+
+    /**
+     * Set Logger Level
+     * @param level Log Level
+     */
+    public static void setLoggingLevel(int level) {
+        enableLogging(level);
     }
 
     /**
      * Disable Logger
      */
     public static void disableLogging() {
-        DEBUG = false;
+        L.level = LEVEL_NONE;
     }
 
     public static void v(String tag, String msg) {
@@ -30,7 +53,7 @@ public class L {
     }
 
     public static void v(String tag, String msg, Throwable t) {
-        if(DEBUG) {
+        if(level <= LEVEL_VERBOSE) {
             Log.v(tag, msg, t);
         }
     }
@@ -40,7 +63,7 @@ public class L {
     }
 
     public static void d(String tag, String msg, Throwable t) {
-        if(DEBUG) {
+        if(level <= LEVEL_DEBUG) {
             Log.d(tag, msg, t);
         }
     }
@@ -50,7 +73,7 @@ public class L {
     }
 
     public static void i(String tag, String msg, Throwable t) {
-        if(DEBUG) {
+        if(level <= LEVEL_INFO) {
             Log.i(tag, msg, t);
         }
     }
@@ -60,7 +83,7 @@ public class L {
     }
 
     public static void w(String tag, String msg, Throwable t) {
-        if(DEBUG) {
+        if(level <= LEVEL_WARN) {
             Log.w(tag, msg, t);
         }
     }
@@ -70,7 +93,7 @@ public class L {
     }
 
     public static void e(String tag, String msg, Throwable t) {
-        if(DEBUG) {
+        if(level <= LEVEL_ERROR) {
             Log.e(tag, msg, t);
         }
     }
